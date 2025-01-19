5129ab28a587f8f76ea3a11f39d7cf4ffd4e5018
raksha-rajpoot <rakraj654@gmail.com>
2025-01-19 19:00:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ function makeid(l) {
 	var char_list="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 	 for(var i=0; i<l; i++){
 		 ren+= char_list.charAt(
-			 Math.floor()
+			 Math.floor(
 				 Math.random()*char_list.length
 			 )
 		 )
 	 }
 	return ran;
-} 
+}       
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
